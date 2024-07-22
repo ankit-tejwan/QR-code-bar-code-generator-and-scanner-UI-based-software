47b50a140367da90e0d6bab0a87b293ca10682c8
Ankit Tejwan <77053184+ankit-tejwan@users.noreply.github.com>
2024-07-22 12:46:45 +0530

    Update QrCode .py

--- QrCode .py
+++ QrCode .py
@@ -1,15 +1,19 @@
 # run CMD or Terminal to install required module 
 # pip install pyqrcode 
-# pip install pypng
-import pyqrcode 
-import png 
-from pyqrcode import QRCode 
+# pip install pillow
+import pyqrcode
+from PIL import Image
+
 # inputData which represents the input information 
 inputData = str(input("copy paste your information  here-->>"))
+
 # Generate QR code with saved info as in above variable name qrcode 
 qrcode= pyqrcode.create(inputData) 
-# Create and save the svg file naming "mysvgfile.svg" 
-qrcode.svg("mysvgfile.svg", scale = 8) 
-#svg file is scalable vector 2D graphic file Which contains the Quality of graphic
+
 # Create and save the png file naming "QRcode.png" 
-qrcode.png('QRcode.png', scale = 6) 
+qrcode.png('QRcode.png', scale = 6)
+
+# Display the QRcode.png using PIL image
+img = Image.open('QRcode.png')
+img.show()
+
